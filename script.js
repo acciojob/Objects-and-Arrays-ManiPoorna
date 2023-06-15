9f3608f9d5250735accab26402291626423c76ae
Mani Poorna <118202451+ManiPoorna@users.noreply.github.com>
2023-06-15 11:13:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const person = {
 };
 
 let team = players;
-let team1 = players.slice(0);
+let team1 = players;
 const cap1 = person;
 // Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
 
